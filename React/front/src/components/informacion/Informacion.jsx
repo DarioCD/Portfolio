c847dc28a56fbfc28b962e4347cb674490ef5970
Dario <dariocd0808@gmail.com>
2023-08-22 00:44:18 +0100

    Add new photo

--- React/front/src/components/informacion/Informacion.jsx
+++ React/front/src/components/informacion/Informacion.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Informacion = () => {
-    const foto = require("../../assets/codeTyping.png")
+    const foto = require("../../assets/fotogolfo.JPG")
   return (
     <div className='containerHomeIntroduction'>
         <div className='conainterInfo'>
